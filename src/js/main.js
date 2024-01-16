430ff082b2edffd4d7be458b292cf2cb613cc2a6
Sangyeon Son <syson16@gmail.com>
2024-01-16 11:43:14 -0500

    dashboard init

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,9 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+// Import JS files
+import './color-modes.js'
+
 const json = require('../json/sample.json');
 
 function component() {
@@ -28,3 +31,54 @@ function jsonText() {
 
 // document.body.appendChild(component());
 // document.body.appendChild(jsonText());
+
+/* globals Chart:false */
+
+(() => {
+	'use strict'
+  
+	// Graphs
+	const ctx = document.getElementById('myChart')
+	// eslint-disable-next-line no-unused-vars
+	const myChart = new Chart(ctx, {
+	  type: 'line',
+	  data: {
+		labels: [
+		  'Sunday',
+		  'Monday',
+		  'Tuesday',
+		  'Wednesday',
+		  'Thursday',
+		  'Friday',
+		  'Saturday'
+		],
+		datasets: [{
+		  data: [
+			15339,
+			21345,
+			18483,
+			24003,
+			23489,
+			24092,
+			12034
+		  ],
+		  lineTension: 0,
+		  backgroundColor: 'transparent',
+		  borderColor: '#007bff',
+		  borderWidth: 4,
+		  pointBackgroundColor: '#007bff'
+		}]
+	  },
+	  options: {
+		plugins: {
+		  legend: {
+			display: false
+		  },
+		  tooltip: {
+			boxPadding: 3
+		  }
+		}
+	  }
+	})
+  })()
+  
